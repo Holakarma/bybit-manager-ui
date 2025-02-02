4cf4ade27a576819f0915224dd74c923899efe39
Holakarma <holakarmos@gmail.com>
2025-02-02 19:43:53 +0500

    Updates

--- assets/not-found-page-hb2HekzU.js
+++
@@ -1,1 +0,0 @@
-import{j as a,f as o,T as s,am as n,an as t}from"./index-CRFM2Fz9.js";const e=()=>a.jsxs(o,{children:[a.jsx(s,{variant:"h1",children:"404. Page was not found"}),"Go to ",a.jsx(n,{to:t.MAIN,children:"main page"}),"."]});export{e as default};
