5b931ff60aafffc0566a6b053af393ffff497804
Holakarma <holakarmos@gmail.com>
2025-02-25 16:44:30 +0500

    Updates

---
+++ assets/not-found-page-CdhMbF6-.js
@@ -0,0 +1,1 @@
+import{j as a,k as o,T as s,ap as n,aq as t}from"./index-DPhf5-PO.js";const e=()=>a.jsxs(o,{children:[a.jsx(s,{variant:"h1",children:"404. Page was not found"}),"Go to ",a.jsx(n,{to:t.MAIN,children:"main page"}),"."]});export{e as default};
