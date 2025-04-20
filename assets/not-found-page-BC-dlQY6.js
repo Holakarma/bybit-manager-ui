52e6df7938fd46a365cc57ca318d29badc847eaa
Holakarma <holakarmos@gmail.com>
2025-04-20 20:28:12 +0500

    Updates

--- assets/not-found-page-BC-dlQY6.js
+++
@@ -1,1 +0,0 @@
-import{j as a,d as o,T as s,am as n,an as t}from"./index-BwkqNw9G.js";const e=()=>a.jsxs(o,{children:[a.jsx(s,{variant:"h1",children:"404. Page was not found"}),"Go to ",a.jsx(n,{to:t.MAIN,children:"main page"}),"."]});export{e as default};
