937bf1581d824acfd181b8333d3a82afa28ed107
Holakarma <holakarmos@gmail.com>
2025-04-23 21:55:49 +0500

    [feat]: prelogin for account preferences added

--- src/features/set-preference/api/setPreferences.js
+++ src/features/set-preference/api/setPreferences.js
@@ -2,33 +2,28 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useAccount } from 'entities/account';
 import { useLogs } from 'entities/log';
 import { usePendingTasks, useTask } from 'entities/task';
+// eslint-disable-next-line no-restricted-imports
+import { useLoginAccountMutation } from 'features/login/api/loginAccount';
 import { Api, deduplicateRequests, ENDPOINTS } from 'shared/api';
 import { getExpireFromAllCookies } from 'shared/lib/session-cookies';
 
 const setPreferences = (database_id, signal) => {
 	const api = new Api();
-	return api.Post(ENDPOINTS.set_preferences, { deposit_to: 'uta' }, {
-		signal,
-		params: { database_id },
-	});
+	return api.Post(
+		ENDPOINTS.set_preferences,
+		{ deposit_to: 'uta' },
+		{
+			signal,
+			params: { database_id },
+		},
+	);
 };
 
 export const useSetPreferencesAccountMutation = () => {
-	const accountMutation = useAccount();
-
 	const mutationFunction = ({ database_id, signal }) => {
 		return deduplicateRequests({
 			requestKey: ['set-preferences', database_id],
 			requestFn: async () => {
-				const account = await accountMutation.mutateAsync(database_id);
-
-				const expire = getExpireFromAllCookies(account.cookies);
-
-				/* If account is not logged in */
-				if (!expire || expire * 1000 < Date.now()) {
-					return { result: null, database_id };
-				}
-
 				const result = await setPreferences(database_id, signal);
 				return { result, database_id };
 			},
@@ -55,11 +50,51 @@ const useSetPreferencesTask = () => {
 		changeAccountDescription(taskId, id, 'Seting preferences...');
 	};
 	const mutation = useSetPreferencesAccountMutation();
+	const loginMutation = useLoginAccountMutation();
+	const accountMutation = useAccount();
 	const addInfoLog = useLogs.use.addInfoLog();
 	const addErrorLog = useLogs.use.addErrorLog();
 	const addSuccessLog = useLogs.use.addSuccessLog();
 
-	const mutationFunction = async ({ database_id, signal, taskId }) => {
+	const mutationFunction = async ({
+		database_id,
+		signal,
+		taskId,
+		settings,
+	}) => {
+		const account = await accountMutation.mutateAsync(database_id);
+		const expire = getExpireFromAllCookies(account.cookies);
+
+		/* If account is not logged in */
+		if (!expire || expire * 1000 < Date.now()) {
+			if (settings.prelogin) {
+				try {
+					addInfoLog({
+						message:
+							'Cookies was not found or expired, try to prelogin account',
+						group: taskId,
+						database_id,
+					});
+
+					await loginMutation.mutateAsync({
+						database_id,
+						taskId,
+						signal,
+					});
+				} catch (error) {
+					addErrorLog({ error, group: taskId, database_id });
+					return;
+				}
+			} else {
+				addErrorLog({
+					error: new Error('Account is not logged in, skip'),
+					group: taskId,
+					database_id,
+				});
+				return;
+			}
+		}
+
 		addInfoLog({
 			message: 'Setting preferences',
 			group: taskId,
