7a57f8adcd5c4e8b4304160a40a1138a824eb27f
Holakarma <holakarmos@gmail.com>
2025-04-25 17:04:11 +0500

    Updates

--- assets/not-found-page-CDXAXga8.js
+++
@@ -1,1 +0,0 @@
-import{j as a,d as o,T as s,ao as n,ap as t}from"./index-DZ54aNOs.js";const e=()=>a.jsxs(o,{children:[a.jsx(s,{variant:"h1",children:"404. Page was not found"}),"Go to ",a.jsx(n,{to:t.MAIN,children:"main page"}),"."]});export{e as default};
