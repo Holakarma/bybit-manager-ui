4b370177640e5b39ea7ae697d1bce9071c6f902d
Holakarma <holakarmos@gmail.com>
2025-05-01 15:08:08 +0500

    Updates

---
+++ assets/not-found-page-C6XlTSvk.js
@@ -0,0 +1,1 @@
+import{j as a,e as o,T as s,aq as n,ar as r}from"./index-BdHuzUY6.js";const e=()=>a.jsxs(o,{children:[a.jsx(s,{variant:"h1",children:"404. Page was not found"}),"Go to ",a.jsx(n,{to:r.MAIN,children:"main page"}),"."]});export{e as default};
--- assets/not-found-page-CiX2HHra.js
+++
@@ -1,1 +0,0 @@
-import{j as a,e as o,T as s,aq as n,ar as r}from"./index-sf5N926f.js";const e=()=>a.jsxs(o,{children:[a.jsx(s,{variant:"h1",children:"404. Page was not found"}),"Go to ",a.jsx(n,{to:r.MAIN,children:"main page"}),"."]});export{e as default};
