623d7f89b2f447e0470a9955548d9156b0dc5b3e
Holakarma <holakarmos@gmail.com>
2025-05-09 16:47:58 +0500

    Updates

--- assets/not-found-page-DFqQaM-1.js
+++
@@ -1,1 +0,0 @@
-import{j as a,e as o,T as s,aF as n,aG as t}from"./index-C3zamMOx.js";const e=()=>a.jsxs(o,{children:[a.jsx(s,{variant:"h1",children:"404. Page was not found"}),"Go to ",a.jsx(n,{to:t.MAIN,children:"main page"}),"."]});export{e as default};
