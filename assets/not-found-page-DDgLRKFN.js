92984732b9d33d83381f110761beae0663ed13fc
Holakarma <holakarmos@gmail.com>
2025-05-12 20:18:02 +0500

    Updates

---
+++ assets/not-found-page-DDgLRKFN.js
@@ -0,0 +1,1 @@
+import{j as a,e as o,T as s,aS as n,aT as t}from"./index-FU8E5BN4.js";const e=()=>a.jsxs(o,{children:[a.jsx(s,{variant:"h1",children:"404. Page was not found"}),"Go to ",a.jsx(n,{to:t.MAIN,children:"main page"}),"."]});export{e as default};
