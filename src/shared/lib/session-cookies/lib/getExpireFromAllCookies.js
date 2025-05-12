4fce46e58880b7687cf0a91dc60683bb9b7ef0ce
Holakarma <holakarmos@gmail.com>
2025-05-12 18:21:03 +0500

    [fix]: fixed cookies bug

--- src/shared/lib/session-cookies/lib/getExpireFromAllCookies.js
+++ src/shared/lib/session-cookies/lib/getExpireFromAllCookies.js
@@ -4,7 +4,7 @@ import getSessionCookies from './getSessionCookies';
 const getExpireFromAllCookies = (cookies, cookieName = 'secure-token') => {
 	const sessionCookies = getSessionCookies(cookies, cookieName);
 
-	if (!sessionCookies.length) return null;
+	if (!sessionCookies?.length) return null;
 
 	return findLatestExpires(sessionCookies);
 };
